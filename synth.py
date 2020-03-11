edcf613f331d530e43ee8ed25fb48d9d702bbd69
Max Sun <maxlouiesun@gmail.com>
2020-03-10 22:20:44 -0700

    working on events

--- synth.py
+++ synth.py
@@ -1,6 +1,6 @@
 
-
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy import signal, fftpack
 from scipy.fft import fftshift
 from typing import NamedTuple, Callable
@@ -11,92 +11,136 @@ import threading
 import json
 import pyaudio
 
-# p = pyaudio.PyAudio()
 
-class Wave:
+class Constant:
     
-    # we want to describe waves as a function of time as well
+    def __init__(self, value):
+        self.amplitude = value
+        
+    def sample(self, start: float, end: float, sample_rate: int=44100):
+        n = math.ceil((end - start) * sample_rate)
+        return self.amplitude
+    
 
-    def __init__(self, frequency=440, amplitude=1, phase=0):
-        self.frequency = frequency # Cycles per Second
+class Osc:
+    
+    def __init__(self, frequency, phase, amplitude):
+        self.waveform = np.sin
+        self.frequency = frequency
+        self.phase = phase
         self.amplitude = amplitude
-        self.phase = phase # Angular offset
-        self.period = 1 / frequency # Seconds per cycle
         
-    def sample(self, duration=1, sample_rate=44100):
-        num_samples = math.ceil(sample_rate * duration)
-        phase_shift = self.phase * np.pi * 2
-        sampletimes =  np.linspace(0, duration, num_samples, endpoint=False)
-        return (self.amplitude * np.sin(sampletimes * 2 * np.pi * self.frequency + phase_shift)).astype(np.float32)
+    def sample(self, start: float, end: float, sample_rate: int=44100):
+        
+        duration = end - start
+        num_samples = math.floor(duration * sample_rate)
+        sampletimes = np.linspace(start, end, num_samples)
+        
+        freq = self.frequency.sample(start, end)
+        phase = self.phase.sample(start, end)
+        phase_shift = np.pi * 2 * phase
+        
+        amp = self.amplitude.sample(start, end)[:len(sampletimes)]
+        print(len(amp), len(sampletimes))
+        x = amp * np.sin(np.pi * 2 * freq * sampletimes + phase_shift)
+        
+        results = x
+        return results.astype(np.float32)
     
 
-def average_samples(samples):
-    total = reduce(lambda a, b: a + b, samples)
-    return np.array(total / len(samples)).astype(np.float32)
-
-
-class Test:
-
-    def __init__(self, p):
-        # attacking is a dict mapping frequency to time remaining on the attack
-        self.pyaudio = p
-        self.isAlive = True
-        self.sample_rate = 44100
-        t = threading.Thread(target=self.audio_loop, args=())
-        t.start()
+class Event:
+    
+    def __init__(self, expr, start: float, duration: float, before, after):
+        self.start = start
+        self.duration = duration
+        self.end = start + duration
+        self.expr = expr
+        self.before = before
+        self.after = after
         
-        # a channel has a frequency, amplitude, phase
-        # can be on or off, and can have effects applied that affect its samples
-        self.frequencies = set()
-        self.attacks = {}
+    def sample(self, start: float, end: float, sample_rate: int=44100):
+        
+        duration = end - start
+        num_samples = math.ceil(duration * sample_rate)
+        
+        mod_start = max(self.start, start)
+        mod_end = min(self.end, end)
+        mod_duration = mod_end - mod_start
 
-    def attack(self, frequency):
-        self.attacks[frequency] = 1
+        
+        print('Mod Start:', mod_start)
+        print('Mod End:', mod_end)
+        print('Mod Duration:', mod_end)
+
+        before_val = self.before.sample(start, end)
+        after_val = self.after.sample(start, end)
 
 
-    def audio_loop(self):
-        stream = self.pyaudio.open(format=pyaudio.paFloat32,
+        if mod_duration < 0:
+            return np.linspace(before_val, before_val, num_samples).astype(np.float32)
+        sampletimes = np.linspace(0, 1, math.ceil(mod_duration * sample_rate))
+        
+        front_padding = np.linspace(before_val, before_val, int(sample_rate * max(0, self.start - start)))
+        back_padding = np.linspace(after_val, after_val, int(sample_rate * max(0, end - self.end)))
+        print('Padding:', len(front_padding), 'back:', len(back_padding))
+
+        result = np.concatenate(
+            [front_padding,
+                self.expr(sampletimes),
+                back_padding])
+
+        return result.astype(np.float32)
+
+
+amp_ex = Event(
+    start=0,
+    duration=1,
+    before=Constant(0),
+    after=Constant(0.5),
+    expr=lambda x: x
+)
+
+sin_a = Osc(
+    frequency=Constant(440),
+    phase=Constant(0),
+    amplitude=amp_ex
+)
+
+amp_ex2 = Event(
+    start=0.5,
+    duration=1,
+    before=Constant(0),
+    after=Constant(0.5),
+    expr=lambda x: x
+)
+sin2 = Osc(
+    frequency=Constant(660),
+    phase=Constant(0),
+    amplitude=amp_ex2
+)
+
+output_io = pyaudio.PyAudio()
+# d = 0.02
+plt.plot(amp_ex.sample(0, 3), label='amp')
+# plt.plot(sin2.sample(0.25, 1), label='oscillator')
+# plt.legend()
+plt.show()
+
+stream = output_io.open(format=pyaudio.paFloat32,
                         channels=1,
-                        rate=self.sample_rate,
+                        rate=44100,
                         output=True)
-        print('looping...')
-        loop_duration = 0.05
-        while self.isAlive:
-            to_merge = []
-            for freq in self.frequencies:
-                w = Wave(frequency=freq)
-                x = w.sample(loop_duration)
-
-                if freq in self.attacks:
-                    atk = 1 - self.attacks[freq]
-                    ``
-
-                to_merge.append(x)
-            if len(to_merge) > 0:
-                s = average_samples(to_merge)
-                stream.write((0.5 * s).tobytes())
-            else:
-                time.sleep(loop_duration)
-            
 
 
+sources = [sin_a, sin2]
 
-        print('ending audio loop')
-        stream.stop_stream()
-        stream.close()
-        self.pyaudio.terminate()
+time = 0
+step = 10
+to_merge = []
+for x in sources:
+    sample = x.sample(time, time + step)
+    to_merge.append(sample)
 
-    def kill(self):
-        self.isAlive = False
+merged = reduce(lambda a, b: a + b, to_merge) / len(to_merge)
 
-
-
-
-t = Test(pyaudio.PyAudio())
-t.frequencies.add(440)
-time.sleep(1)
-t.frequencies.remove(440)
-t.frequencies.add(220)
-time.sleep(2)
-t.kill()
-# p.terminate()
+stream.write(merged.tobytes())
